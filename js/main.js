e30c7f27e080b47ec9ccc1fbd0e18ce733f06c1c
Chris <Chrisito1127@gmail.com>
2023-09-30 21:21:15 -0400

    3x3 Grid

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log('Linked up')
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+let board; 
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+function init(){
+    board = [
+        'i', 'i', 'i',
+        'i', 'i', 'i',
+        'i', 'i', 'i'
+    ];
+    render();
+};
+
+init();
+
+function render(){
+
+    board.forEach(function(mark, index){
+        console.log(mark, index);
+    });
+} ;
