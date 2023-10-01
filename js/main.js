9e77edcf8e404d056f8b09a2b15af885113954ce
Chris <Chrisito1127@gmail.com>
2023-10-01 09:32:01 -0400

    Win logic

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@
 
 /*----- app's state (variables) -----*/
 let board; 
+let turn = 'X';
+let win;
+
+win = board[0] && board [0] === board[1] && board[0] === board[2] ? board[0]: null;
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'))
+const messages = document.querySelector('h2');
 /*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn);
 /*----- functions -----*/
 function init(){
     board = [
@@ -22,4 +28,16 @@ function render(){
     board.forEach(function(mark, index){
         squares[index].textContent = mark;
     });
+    messages.textContent=`It's ${turn}'s turn`;
 } ;
+
+function handleTurn(event){
+    let idx = squares.findIndex(function(square){
+        return square === event.target;
+    })
+
+    board[idx] = turn;
+
+    turn = turn === 'X' ? 'O': 'X';
+    render();
+};
