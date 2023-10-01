28345dde1de0738b826e2551b5a5f71d6a4bc034
Chris <Chrisito1127@gmail.com>
2023-09-30 21:36:41 -0400

    Take turns

--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@
 /*----- app's state (variables) -----*/
 let board; 
 /*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'))
 /*----- event listeners -----*/
 /*----- functions -----*/
 function init(){
     board = [
-        'i', 'i', 'i',
-        'i', 'i', 'i',
-        'i', 'i', 'i'
+        '', '', '',
+        '', '', '',
+        '', '', ''
     ];
     render();
 };
@@ -19,6 +20,6 @@ init();
 function render(){
 
     board.forEach(function(mark, index){
-        console.log(mark, index);
+        squares[index].textContent = mark;
     });
 } ;
