2b50653a61c72b1be03dc268f8fea96b3d05978b
Chris <Chrisito1127@gmail.com>
2023-10-01 09:50:41 -0400

    The game is finished

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 /*----- constants -----*/
-
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 /*----- app's state (variables) -----*/
 let board; 
 let turn = 'X';
 let win;
 
-win = board[0] && board [0] === board[1] && board[0] === board[2] ? board[0]: null;
+
 /*----- cached element references -----*/
-const squares = Array.from(document.querySelectorAll('#board div'))
-const messages = document.querySelector('h2');
+const squares = Array.from(document.querySelectorAll('#board div'));
+
 /*----- event listeners -----*/
 document.getElementById('board').addEventListener('click', handleTurn);
+const messages = document.querySelector('h2');
 /*----- functions -----*/
 function init(){
     board = [
@@ -21,23 +31,32 @@ function init(){
     render();
 };
 
-init();
-
 function render(){
-
     board.forEach(function(mark, index){
         squares[index].textContent = mark;
     });
     messages.textContent=`It's ${turn}'s turn`;
-} ;
+};
 
 function handleTurn(event){
     let idx = squares.findIndex(function(square){
         return square === event.target;
-    })
+    });
 
     board[idx] = turn;
-
+    console.log(board);
     turn = turn === 'X' ? 'O': 'X';
+    win = getWinner()
     render();
 };
+
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+      });
+      return winner;
+    
+}
+
+init();
